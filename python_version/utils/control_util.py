dadb26d8d09f61c16cbcc2b77e45bf353bb499e4
Juergens MacBookPro <loebbeckejohannes@hotmail.de>
2024-11-27 09:05:10 +0100

    updated tester

--- python_version/utils/control_util.py
+++ python_version/utils/control_util.py
@@ -96,13 +96,35 @@ def directly_follows_can(root, xpath1, xpath2):
     ancestors1, ancestors2, shared_ancestors = get_shared_ancestors(root, xpath1, xpath2)
     shared_parallel = False
     shared_branch = False
+    shared_exclusive = False
     for ancestor in shared_ancestors:
-        if ancestor.tag.endswith("parrallel"):
-            return True ## if  in parallel calls they could always follow each other
+        if ancestor.tag.endswith("choose"):
+            shared_exclusive = True
+        if ancestor.tag.endswith("parallel_branch"):
+            shared_parallel = True
+        if ancestor.tag.endswith("parallel"):
+            if not shared_parallel:
+                return True
         if ancestor.tag.endswith("otherwise") or ancestor.tag.endswith("alternative"):
             shared_branch = True 
         if ancestor.tag.endswith("choose") and not shared_branch:
            return False ## if on different branches in exclusive, they can never follow each other
-
+    ele1 = root.find(xpath1, namespace)
+    ele2 = root.find(xpath2, namespace)
+    elements = [elem for elem in root.iter() if elem.tag.endswith('call')]
+    for i in range(len(elements) - 1):
+        if elements[i] is ele1 and elements[i + 1] is ele2:
+            return True
+    last_in_branch = False
+    for ancestor in ancestors1:
+        if ancestor.tag.endswith("parallel") or ancestor.tag.endswith("choose"):
+            elementsall = [elem for elem in root.iter() if elem.tag.endswith("call") or elem.tag.endswith("parallel") or elem.tag.endswith("choose") or elem.tag.endswith("parallel_branch") or elem.tag.endswith("alternative") or elem.tag.endswith("otherwise")]
+            for i in range(len(elementsall)-1):
+                if elementsall[i] is ele1:
+                    if not elementsall[i + 1].tag.endswith("call"):
+                        last_in_branch = True
+                if elementsall[i] is ele2:
+                    return last_in_branch and not shared_exclusive
+    return False
 
 
