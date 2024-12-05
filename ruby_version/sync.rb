fd4fd7547332a6b57eeba9729d1eda65601be565
Juergens MacBookPro <loebbeckejohannes@hotmail.de>
2024-12-05 14:41:33 +0100

    fixed sync.rb

--- ruby_version/sync.rb
+++ ruby_version/sync.rb
@@ -17,30 +17,27 @@ end
 # accepts timestamp, waits until the timestamp to sync up 
 post '/wait_until' do
   content_type :json
-
+  
   # Parse the request body for the timestamp
-  request_payload = JSON.parse(request.body.read)
-  timestamp = request_payload['timestamp']
-
+  timestamp = params["timestamp"]
   # Convert the timestamp to a Time object
   begin
-    target_time = Time.parse_time(timestamp)
+    target_time = parse_time(timestamp)
   rescue ArgumentError
     halt 400, { error: 'Invalid timestamp format' }.to_json
   end
-
   # Calculate the delay in seconds
   delay = target_time - Time.now
 
   # If the target time is in the past, return immediately
   if delay <= 0
-    { waited: False }.to_json
+    { waited: false }.to_json
   else
     # Wait until the specified time
     sleep(delay)
 
     # Return a response
-    { waited: True }.to_json
+    { waited: true }.to_json
   end
 end
 
