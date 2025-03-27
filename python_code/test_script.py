546ba69f3d651d57fb789dc65efee66f3a1d55e6
Johannes Loe <61540624+JohannesLbck@users.noreply.github.com>
2025-03-27 13:03:48 +0100

    Change Comment

--- python_code/test_script.py
+++ python_code/test_script.py
@@ -1,5 +1,5 @@
 #! /usr/bin/python
-#    Copyright (C) <2025>  <Johannes Löbbecke>
+#    Copyright (C) <2025>  <>
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
