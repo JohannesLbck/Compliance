e92bf0257b6edbfe5178194ee580272c666d3b39
Juergens MacBookPro <loebbeckejohannes@hotmail.de>
2025-05-14 10:08:50 +0200

    added updated_verification

--- python_code/updated_verification.py
+++ python_code/updated_verification.py
@@ -15,7 +15,11 @@ logger = logging.getLogger(__name__)
 ## Existence: Checks if an activity a exists in the xml tree and returns the element or None, identifes by label, to identify using resource/data see below
 def exists(tree, a):#
     if isinstance(a, ET.Element):
-        return a
+        if a in tree:
+            return a
+        else:
+            logger.warning(f'The existence of a element {a} was checked in the tree but not found, this could be a error in the code, a is returned to avoid further type errors')
+            return a
     elif a == "End Activity" or a == "end activity":
         return tree.find(".//ns0:end_activity", namespace)
     elif a == "Start Activity" or a == "start activity":
@@ -323,7 +327,7 @@ def by_due_date_explicit(tree, a, timestamp):
             if int(call[1]) <= int(timestamp):
                 condition = f"data.{call[2]}"
                 logger.info(f'found a due date activity that enforces the date requirement, check for alternative branch with condition: "{condition}" that eventually leads to "{a}"')
-                return data_value_alternative_eventually_follows(tree, condition, a)
+                return condition_eventually_follows(tree, condition, a)
         logger.info(f'no due date activity was found to enforce the due date requirement')
         return False
     else:
@@ -439,7 +443,7 @@ def activity_receives(tree, a, data):
         logger.info(f'Activity "{a}" does not exist in the tree, accordingly the receive is trivally true')
         return True
     
-def data_value_alternative(tree, condition):
+def condition(tree, condition):
     if condition_finder(tree, condition):
         return True
     else:
@@ -466,12 +470,12 @@ def condition_directly_follows(tree, condition , a):
             logger.info(f'Activity "{a}" directly followed the data_condition "{condition}"')
             return True
         counter += 1
-    logger.error(f"IF we got here something went wrong, in the data_value_alternative_directly_follows function")
+    logger.error(f"IF we got here something went wrong, in the condition_directly_follows function")
     return False
 
 
 ## Eventually follows a data condition. The default here is to check in the same branch (see scope = "branch") if the scope is said to global it checks anywhere after the branch as well 
-def data_value_alternative_eventually_follows(tree, condition, a, scope = "branch"):
+def condition_eventually_follows(tree, condition, a, scope = "branch"):
     branch = condition_finder(tree, condition)
     if branch is not None:
         apath = exists(branch,a)
@@ -506,8 +510,8 @@ def data_value_alternative_eventually_follows(tree, condition, a, scope = "branc
     else:
         logger.info(f'No branch with condition: "{condition}" was found')
         return False
-def data_leads_to_absence(tree, condition, a):
-    return not data_value_alternative_eventually_follows(tree, condition, a)
+def condition_absence(tree, condition, a):
+    return not condition_eventually_follows(tree, condition, a)
 
 ## Obligations vs Permissions: These can be modeled on the requirements side, using ands, ors and by just included the rule or not
 ## Complex resource requirements: These can also be modeled on the requirements side usind ands, ors and by just including rule or not
