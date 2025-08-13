65bd5ab2d834f5f14a79ae7964d0f489fb123ff6
juergensmacbookpro <loebbeckejohannes@hotmail.de>
2025-08-13 10:30:24 +0200

    updated licensing

--- python_code/utils/control_util.py
+++ python_code/utils/control_util.py
@@ -1,4 +1,4 @@
-#    Copyright (C) <2025>  <Author>
+#    Copyright (C) <2025>  <Johannes Löbbecke>
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
